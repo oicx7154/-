f02dd23eaa341df274a91dcfdbc97dce23b6721a
oicx7154 <oicx7154@cpaar.com>
2026-04-04 14:35:37 +0800

    Update announcements.lua

--- announcements.lua
+++ announcements.lua
@@ -1,5 +1,5 @@
 return {
 Title = "灵构脚本",
-announcements = {"2026/4/4\n更新/Update:\n新增游戏/New Game:\n撕咬之夜\Bite By Night",
+announcements = {"2026/4/4\n更新/Update:\n新增游戏/New Game:\n撕咬之夜/Bite By Night",
   }
 }
